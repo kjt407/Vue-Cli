88f02a24d569a91c080200f26faec00e8a24081d
kjt407 <kjt40700@gmail.com>
2022-10-27 23:25:10 +0900

    Vuetify 레이아웃 연습

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <v-app>
-    <v-container>
-      <v-layout row wrap>
-        <v-flex xs12>
-          <User></User>
-        </v-flex>
-      </v-layout>
+    <v-container class="grey">
+        <!-- <v-flex xs12> -->
+          <User v-if="index = 0"></User>
+          <Grid v-else-if="index = 1"></Grid>
+        <!-- </v-flex> -->
     </v-container>
   </v-app>
 </template>
 
 <script>
-import User from './components/sample1/User.vue' ;
-import HomeView from './views/HomeView.vue' ;
+import User from './components/sample1/User' ;
+import HomeView from './views/HomeView' ;
+import Grid from './components/sample_layout/Grid';
 
 export default {
   components: {
     User,
-    HomeView  }
+    HomeView,
+    Grid
+},
+  data() {
+    return {
+      index: 1,
+    }
+  }
   
 }
 </script>
---
+++ src/components/sample_layout/Grid.vue
@@ -0,0 +1,54 @@
+<template>
+<div class="pa-2">
+    <v-row dense :justify="'center'">
+        <v-col cols="12">
+            <v-row no-gutters>
+                <v-col cols="auto" class="mr-auto">
+                    <div>
+                        <span>메인 텍스트 입니다.메인 텍스트 입니다.메인 텍스트 입니다.메인 텍스트 입니다.</span>
+                    </div>
+                </v-col>
+                <v-col cols="auto" class="">
+                    <v-btn class="mr-2">
+                        버튼
+                    </v-btn>
+                    <v-btn>
+                        버튼
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-col>
+        <v-col cols="12">
+            <v-row dense>
+                <v-col cols="12">
+                    <Card></Card>
+                </v-col>
+                <v-col cols="12" md="4">
+                    <Card></Card>
+                </v-col>
+                <v-col cols="12" md="4">
+                    <Card></Card>
+                </v-col>
+                <v-col cols="12" md="4">
+                    <Card></Card>
+                </v-col>
+            </v-row>
+        </v-col>
+        <v-col cols="12">
+            <Card></Card>
+        </v-col>
+    </v-row>
+</div>
+</template>
+
+<script>
+import Card from './views/Card'
+export default {
+    components: { Card },
+    data () {
+        return {
+        
+        }
+    }
+}
+</script>
